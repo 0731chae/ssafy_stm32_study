a434a89e0eb7d0231a8407cd6554a70dfc3bd1fa
moonsyu <anstn0104@naver.com>
2026-08-25 23:23:07 +0900

    add: UART main.cpp

--- week4/moonsu/main.c
+++ week4/moonsu/main.c
@@ -1,216 +1,386 @@
-#include "stm32f446xx.h"
-#include <stdint.h>
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
 
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+TIM_HandleTypeDef htim2;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+uint32_t status = 0;
+uint32_t ready_time;
+uint32_t start_time, end_time;
+float duration_us, distance;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM2_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+static void DWT_Init(void);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void delay_us(uint32_t us) {
+	  TIM2->CNT = 0;
+	  while (TIM2->CNT < us) {
+
+	  }
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
-    volatile uint32_t echo_time = 0U;
-    volatile uint32_t distance = 0U;
-    uint8_t status = 0U;
 
-    /*
-     * 시스템 클럭을 HSI 16MHz로 설정
-     */
-    RCC->CR |= RCC_CR_HSION;
+  /* USER CODE BEGIN 1 */
 
-    while ((RCC->CR & RCC_CR_HSIRDY) == 0U)
-    {
-    }
+  /* USER CODE END 1 */
 
-    /* 시스템 클럭을 HSI로 전환 */
-    RCC->CFGR &= ~RCC_CFGR_SW;
+  /* MCU Configuration--------------------------------------------------------*/
 
-    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI)
-    {
-    }
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-    /* AHB, APB1, APB2 Prescaler = 1 */
-    RCC->CFGR &= ~(RCC_CFGR_HPRE |
-        RCC_CFGR_PPRE1 |
-        RCC_CFGR_PPRE2);
+  /* USER CODE BEGIN Init */
 
-    /*
-     * GPIOA, GPIOB 클럭 활성화
-     */
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+  /* USER CODE END Init */
 
-    /*
-     * TIM2 클럭 활성화
-     * TIM2는 APB1 버스
-     */
-    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+  /* Configure the system clock */
+  SystemClock_Config();
 
-    /* 클럭 활성화 완료 대기 */
-    (void)RCC->AHB1ENR;
-    (void)RCC->APB1ENR;
+  /* USER CODE BEGIN SysInit */
 
-    /*
-     * PB10: 초음파 TRIG 출력
-     * MODER10 = 01
-     */
-    GPIOB->MODER &= ~(3U << (10U * 2U));
-    GPIOB->MODER |= (1U << (10U * 2U));
+  /* USER CODE END SysInit */
 
-    /*
-     * PA8: 초음파 ECHO 입력
-     * MODER8 = 00
-     */
-    GPIOA->MODER &= ~(3U << (8U * 2U));
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM2_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+  DWT_Init();
 
-    /* PA8 Pull-down */
-    GPIOA->PUPDR &= ~(3U << (8U * 2U));
-    GPIOA->PUPDR |= (2U << (8U * 2U));
+  if (HAL_TIM_Base_Start(&htim2) != HAL_OK) {
+	  Error_Handler();
+  }
 
-    /*
-     * PB1: LED 출력
-     * MODER1 = 01
-     */
-    GPIOB->MODER &= ~(3U << (1U * 2U));
-    GPIOB->MODER |= (1U << (1U * 2U));
 
-    /* PB10, PB1 Push-Pull */
-    GPIOB->OTYPER &= ~(1U << 10U);
-    GPIOB->OTYPER &= ~(1U << 1U);
+  static const uint8_t msg[] = "UART TEST OK\r\n";
 
-    /* 출력 속도 Low Speed */
-    GPIOB->OSPEEDR &= ~(3U << (10U * 2U));
-    GPIOB->OSPEEDR &= ~(3U << (1U * 2U));
+  /* USER CODE END 2 */
 
-    /* TRIG LOW */
-    GPIOB->BSRR = (1U << (10U + 16U));
+  /* Initialize leds */
+  BSP_LED_Init(LED2);
 
-    /* LED OFF: Active-High LED 기준 */
-    GPIOB->BSRR = (1U << (1U + 16U));
+  /* Initialize USER push-button, will be used to trigger an interrupt each time it's pressed.*/
+  BSP_PB_Init(BUTTON_USER, BUTTON_MODE_EXTI);
 
-    /*
-     * TIM2 설정
-     *
-     * 타이머 클럭: 16MHz
-     * 16MHz / (15 + 1) = 1MHz
-     * 1카운트 = 1us
-     */
-    TIM2->CR1 = 0U;
-    TIM2->PSC = 15U;
-    TIM2->ARR = 0xFFFFFFFFU;
-    TIM2->CNT = 0U;
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
 
-    /* Prescaler 적용 */
-    TIM2->EGR = TIM_EGR_UG;
-    TIM2->SR = 0U;
+    /* USER CODE END WHILE */
 
-    /* TIM2 시작 */
-    TIM2->CR1 |= TIM_CR1_CEN;
+    /* USER CODE BEGIN 3 */
+	  HAL_UART_Transmit(
+	      &huart2,
+	      (uint8_t *)msg,
+	      sizeof(msg) - 1,
+	      HAL_MAX_DELAY
+	  );
 
-    while (1)
-    {
-        status = 0U;
-        echo_time = 0U;
-        distance = 0U;
-
-        /*
-         * TRIG LOW 2us
-         */
-        GPIOB->BSRR = (1U << (10U + 16U));
-
-        TIM2->CNT = 0U;
-
-        while (TIM2->CNT < 2U)
-        {
-        }
-
-        /*
-         * TRIG HIGH 10us
-         */
-        GPIOB->BSRR = (1U << 10U);
-
-        TIM2->CNT = 0U;
-
-        while (TIM2->CNT < 10U)
-        {
-        }
-
-        /*
-         * TRIG LOW
-         */
-        GPIOB->BSRR = (1U << (10U + 16U));
-
-        /*
-         * PA8 ECHO가 HIGH가 될 때까지 대기
-         * 최대 30ms
-         */
-        TIM2->CNT = 0U;
-
-        while (((GPIOA->IDR & (1U << 8U)) == 0U) &&
-            (TIM2->CNT < 30000U))
-        {
-        }
-
-        /*
-         * ECHO가 정상적으로 HIGH가 됐는지 확인
-         */
-        if (((GPIOA->IDR & (1U << 8U)) != 0U) &&
-            (TIM2->CNT < 30000U))
-        {
-            /*
-             * ECHO HIGH 시간 측정
-             */
-            TIM2->CNT = 0U;
-
-            while (((GPIOA->IDR & (1U << 8U)) != 0U) &&
-                (TIM2->CNT < 30000U))
-            {
-            }
-
-            echo_time = TIM2->CNT;
-
-            /*
-             * ECHO가 정상적으로 LOW로 돌아온 경우
-             */
-            if (((GPIOA->IDR & (1U << 8U)) == 0U) &&
-                (echo_time < 30000U))
-            {
-                /*
-                 * 거리 계산
-                 * 단위: cm
-                 */
-                distance = echo_time / 58U;
-
-                /*
-                 * 10cm 이하: LED OFF
-                 * 11cm 이상: LED ON
-                 */
-                if (distance >= 11U)
-                {
-                    status = 1U;
-                }
-                else
-                {
-                    status = 0U;
-                }
-            }
-        }
-
-        /*
-         * PB1 LED 제어
-         */
-        if (status != 0U)
-        {
-            /* LED ON */
-            GPIOB->BSRR = (1U << 1U);
-        }
-        else
-        {
-            /* LED OFF */
-            GPIOB->BSRR = (1U << (1U + 16U));
-        }
-
-        /*
-         * 다음 측정까지 60ms 대기
-         */
-        TIM2->CNT = 0U;
-
-        while (TIM2->CNT < 60000U)
-        {
-        }
-    }
+	  HAL_Delay(1000);
+  }
+  /* USER CODE END 3 */
 }
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 83;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 4294967295;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_3|GPIO_PIN_8, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1|GPIO_PIN_10, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PC2 PC9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PC3 PC8 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA6 PA7 PA8 */
+  GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB1 PB10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA9 PA10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  GPIO_InitStruct.Pin = GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = GPIO_PIN_2;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+static void DWT_Init(void)
+{
+    CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+    DWT->CYCCNT = 0U;
+    DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
